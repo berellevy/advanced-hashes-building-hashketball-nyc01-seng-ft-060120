9197b175dab0679985be38223ea1c69a4bde7857
Berel Levy <berellevy@gmail.com>
2020-05-22 18:56:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,13 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: []
+      players: [
+        {},
+        {},
+        {},
+        {},
+        {}
+      ]
     },
     away: {
       team_name: "Charlotte Hornets",
