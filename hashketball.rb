6d0e3e18ffd0a3525247ce2f7e7fbe999b6c9a2f
Berel Levy <berellevy@gmail.com>
2020-05-22 19:09:37 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,14 +51,14 @@ def game_hash
         },
         {
           player_name: "Jason Terry",
-          number: 00,
-          shoe: 00,
-          points: 00,
-          rebounds: 00,
-          assists: 00,
-          steals: 00,
-          blocks: 00,
-          slam_dunks: 00
+          number: 1,
+          shoe: 2,
+          points: 2,
+          rebounds: 4,
+          assists: 11,
+          steals: 15,
+          blocks: 19,
+          slam_dunks: 31
         }
       ]
     },
